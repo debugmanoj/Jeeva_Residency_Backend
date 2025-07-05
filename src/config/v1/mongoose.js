a585222c6b92b8fff5383561a83f15df93fc41bc
debugmanoj <manojkumarwork01@gmail.com>
2025-07-05 21:13:23 +0530

    Fixed_mongodb and verceljson

--- src/config/v1/mongoose.js
+++ src/config/v1/mongoose.js
@@ -1,7 +1,29 @@
-import dotenv from "dotenv";
-dotenv.config();
 import mongoose from "mongoose";
-mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const MONGODB_URI = `${process.env.DB_URL}/${process.env.DB_NAME}`;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the DB_URL and DB_NAME environment variables in .env"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+if (!cached.conn) {
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+  cached.conn = await cached.promise;
+}
 
 export default mongoose;
